3f854d33be010e26b40343e24b54fe57f23f6e23
rubensrvsc <Rubensspfc100@gmail.com>
2021-05-12 15:47:17 -0300

    refactor: TaskSerializerCreate and TaskSerializerList

--- apptodo/serializers.py
+++ apptodo/serializers.py
@@ -6,29 +6,9 @@ class TaskSerializerCreate(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['name','description']
-    
-    def validate_name(self,data):
-        if data == "":
-            return serializers.ValidationError("Campo nome não pode ser vazio")
-        return data
-    
-    def validate_description(self,data):
-        if data == "":
-            return serializers.ValidationError("Campo description não pode ser vazio")
-        return data
 
 class TaskSerializerList(serializers.ModelSerializer):
 
     class Meta:
         model = Task
         fields = ['name','description']
-    
-    def validate_name(self,data):
-        if data == "":
-            return serializers.ValidationError("Campo nome não pode ser vazio")
-        return data
-    
-    def validate_description(self,data):
-        if data == "":
-            return serializers.ValidationError("Campo description não pode ser vazio")
-        return data
